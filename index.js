2ea8eefc6adf3f63445daf994f071af41fb5045d
1992ndPrashanna <prashanna1992@outlook.com>
2022-02-14 19:05:25 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+var http = require("http");
+
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+
+
+var io = require("socket.io")(server);
+// middleware
+app.use(express.json());
+var clients = {};
+
+io.on("connection",(socket)=>{
+
+  console.log(socket.id ," has connected");
+  socket.on("/test",(msg)=>{ 
+    console.log(msg);
+  });
+
+  socket.on("verifyUser",(id)=>{
+    clients[id] = socket;
+  });
+
+  socket.on("sendMessage",(request)=>{
+    let destinationId = request.destinationId;
+    if(clients[destinationId]){
+      clients[destinationId].emit("sendMessage",request); 
+    }
+  });
+});
+
+app.route("/check").get((req,res)=>{
+  return res.json("Your app is running!");
+});
+
+server.listen(port,"0.0.0.0",()=>{console.log("Server Started! at " + port)});
+
